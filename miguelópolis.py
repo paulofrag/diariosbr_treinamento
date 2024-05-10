1096d0e6964315a74eb07901d061574f3c30fe8b
paulofrag <p.fragoso@unesp.br>
2024-05-10 18:58:27 -0300

    tentativa de fazer o download e o trocar de pagina e entender o problema do python porque so aparece o primeio item

--- miguelópolis.py
+++ miguelópolis.py
@@ -16,30 +16,40 @@ def acessar_pagina_dinamica(link):
     navegador = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     navegador.get(link)
     # clicar no download 
+    lista_download= navegador.find_element (By.XPATH,"//span [@class= 'i class=fa fa-download></i']")
+    for diario in lista_download:
+        download = diario
+        print(len(download))
     # find_element e find_elements
     lista_data= navegador.find_elements(By.XPATH,"//span [@class='font-weight-light text-dark']")
     print(len(lista_data))
     for diario in lista_data: 
         
-        # pegar data e quantidade de páginas
+        #pegar data e quantidade de páginas
         data = diario
         print (data.text)
         # pegar número da edição e ano.
-    lista_edição= navegador.find_elements (By.XPATH, "//span [@class='h4 class=mb-0']")
+    lista_edição= navegador.find_elements (By.XPATH,"//span [@class='h4 class=mb-0']")
+    print(lista_edição)
     for diario in lista_edição:
-        print (diario)
+        edição = diario
+        print (edição.text)
         
         
         # regular ou extraordinária
     lista_tipo = navegador.find_elements (By.XPATH, "//span [@class= 'h4 class=mt-0']")
     for diario in lista_tipo:
         print (diario)
-        
-        
 
         #lista_diarios= navegador.find_element (By.XPATH,"//*[@id='formDiarioOficial:listaArquivos_content']/div[1]/div[1]/div/div/div/div/div[1]/div/h4[2]")
     
         # clicar na página seguinte
+    lista_páginaweb = navegador.find_element (By.XPATH, "//span [@class= 'ui-icon ui-icon-seek-next']")
+    for diario in lista_páginaweb:
+            click (diario)
+
+
+    #<a href="#" class="ui-paginator-next ui-state-default ui-corner-all" aria-label="Next Page" tabindex="0"><span class="ui-icon ui-icon-seek-next">N</span></a>
         #lista_diarios= navegador.find_element (By.XPATH,"//*[@id='formDiarioOficial:listaArquivos_paginator_bottom']/span[1]/a[1]")
 
 def main():
